15ddf6f2a30253b76af07033e5f71aaf1a32a1a5
sxbajwa <saiyoxsai@gmail.com>
2025-07-29 19:17:02 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function updateTitle() {
+  document.getElementById("pageTitle").textContent = "I changed my title.";
+}
+
+function showWeather() {
+  document.getElementById("Weather").textContent = "today is my sunnyday";
+}
