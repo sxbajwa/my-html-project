64a86d235e8a7dc4664e991934416b36e5e49a0b
sxbajwa <saiyoxsai@gmail.com>
2025-07-29 19:36:29 +0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function updateTitle() {
-  document.getElementById("pageTitle").textContent = "I changed my title.";
-}
-
-function showWeather() {
-  document.getElementById("Weather").textContent = "today is my sunnyday";
-}
